3621f2fe0f048a7b4ae5c3ddaede16776cd1efef
dpreis <dpreis@charlotte.edu>
2026-04-22 15:26:23 -0400

    feature: Added welcome message on blog posts page and set up login/logout button, both of these use conditional rendering based on login auth state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { ThemeContext } from "./ThemeContext";
 
+// import { useUsername, useAuth } from (path to AuthContext)
+
 function Header() {
   const { theme, toggleTheme } = useContext(ThemeContext);
+  // const username = useUsername(); // Example of using the AuthContext
+  // const { logout } = useAuth(); // Example of using the AuthContext
 
   // Sync theme with body class for global styles
- 
+
   return (
     <div
       className={`header p-4 flex justify-between items-center ${
@@ -19,12 +23,19 @@ function Header() {
         <Link to="/">Home</Link>
         {/* <Link to="/posts">Posts</Link> */}
         <Link to="/contact">Contact</Link>
-        <button
-          onClick={toggleTheme}
-          className="ml-4 px-2 py-1 border rounded"
-        >
+        <button onClick={toggleTheme} className="ml-4 px-2 py-1 border rounded">
           {theme === "light" ? "Dark Mode" : "Light Mode"}
         </button>
+
+        {/* {username ? (
+          <button className="log" onClick={logout}>
+            Logout
+          </button>
+        ) : (
+          <button className="log">
+            <Link to="/login">Login</Link>
+          </button>
+        )} */}
       </nav>
     </div>
   );
