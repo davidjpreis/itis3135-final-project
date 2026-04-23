e11688dcf0a29fc9b6811dc857f9aca29f52d2b7
dpreis <dpreis@charlotte.edu>
2026-04-23 13:09:48 -0400

    small fix in AuthProvider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { AuthContext } from "./AuthContext";
+import AuthContext from "./AuthContext";
 
 function AuthProvider({ children }) {
   const [user, setUser] = useState(() => {
